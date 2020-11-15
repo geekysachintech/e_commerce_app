1e56947401c74bfe8067ac7af0e59161b64ca400
geekysachintech <geekysachin.info@gmail.com>
2020-11-16 03:28:38 +0530

    first commit

---
+++ lib/screens/user_product_screen.dart
@@ -0,0 +1,44 @@
+import 'package:e_commerce_app/providers/products.dart';
+import 'package:e_commerce_app/screens/edit_product_screen.dart';
+import 'package:e_commerce_app/widgets/app_drawer.dart';
+import 'package:e_commerce_app/widgets/user_product_item.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user-products';
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Products'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
+            },
+          ),
+        ],
+      ),
+      drawer: AppDrawer(),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListView.builder(
+          itemCount: productsData.items.length,
+          itemBuilder: (_, i) => Column(
+            children: [
+              UserProductItem(
+                productsData.items[i].id,
+                productsData.items[i].title,
+                productsData.items[i].imageUrl,
+              ),
+              Divider(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
